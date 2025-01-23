3cc787660343d4e283cd77654f3e09b198e3f0cb
Toff-cowan <cowanchristoff24@gmail.com>
2025-01-23 16:20:48 -0500

    Hardware Section Complete

--- hardware/hardware.ino
+++ hardware/hardware.ino
@@ -41,14 +41,14 @@
 
 
 // MQTT CLIENT CONFIG  
-static const char* pubtopic       = "620012345";                    // Add your ID number here
-static const char* subtopic[]     = {"620012345_sub","/elet2415"};  // Array of Topics(Strings) to subscribe to
+static const char* pubtopic       = "620164419";                    // Add your ID number here
+static const char* subtopic[]     = {"620164419_sub","/elet2415"};  // Array of Topics(Strings) to subscribe to
 static const char* mqtt_server    = "address or ip";                // Broker IP address or Domain name as a String 
 static uint16_t mqtt_port         = 1883;
 
 // WIFI CREDENTIALS
-const char* ssid                  = "YOUR_SSID"; // Add your Wi-Fi ssid
-const char* password              = "YOUR_PASS"; // Add your Wi-Fi password 
+const char* ssid                  = 138SL-Residents.; // Add your Wi-Fi ssid
+const char* password              = residents2020@138sl; // Add your Wi-Fi password 
 
 
 
