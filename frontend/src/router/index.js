50a2404dcd3108a97c1ac371fe9388de9d801d0f
Toff-cowan <cowanchristoff24@gmail.com>
2025-01-29 16:14:13 -0500

    Intro Edited

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -3,7 +3,7 @@ import { createRouter, createWebHistory } from 'vue-router'
 
 const routes = [
   { path: '/', name: 'Home', component: () => import('@/views/Home.vue'), meta:{ transition: 'fade'} }, 
-  { path: '/intro', name: 'Intro', component: () => import('@/views/intro.vue'), meta:{ transition: 'fade'} } 
+  { path: '/intro', name: 'Intro', component: () => import('@/views/Intro.vue'), meta:{ transition: 'fade'} } 
 ]
 
 const router = createRouter({
